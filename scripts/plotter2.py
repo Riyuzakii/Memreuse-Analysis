280bc4df18898d51a3c9057862a853ac65c0072d
Riyuzakii <riyuzakiiitk@gmail.com>
2019-09-25 14:01:03 +0530

    Fix plotting script

--- scripts/plotter2.py
+++ scripts/plotter2.py
@@ -1,28 +1,29 @@
 import sys, os
 import numpy as np 
 import matplotlib.pyplot as plt
-fig = plt.figure()
-ax = plt.axes()
+# from pathlib import Path
+directory = "../trace2/"
+for root, dirs, files in os.walk(directory):
+	for file in files:
+		fig = plt.figure()
+		ax = plt.axes()
+		print file
+		# sys.exit(0)
+		filename = directory+ file
+		print filename
+		file_data = np.loadtxt(filename, delimiter=': ', dtype=float, usecols=(1))
+		xaxis = np.loadtxt(filename, delimiter=': ', dtype=float, usecols=(0))
 
-filename = sys.argv[1]
-print filename
-file = np.loadtxt(filename, delimiter=': ', dtype=float, usecols=(1))
-xaxis = np.loadtxt(filename, delimiter=': ', dtype=float, usecols=(0))
-
-# print len(file)
-# f_dist = []
-print file
-# sys.exit(0)
-x = np.linspace(0, len(file), len(file))
-# N = file[len(file)-1]
-
-print file
-plt.plot(xaxis, file)
-# plt.xscale('linear')
-plt.xscale('log')
-title = sys.argv[1]
-fig.suptitle(title[10:-4])
-plt.xlabel('Access Distance')
-plt.ylabel('Cumulative Distance Function')
-plt.savefig(title[10:-4]+'.pdf')
-# plt.show()
+		x = np.linspace(0, len(file_data), len(file_data))
+		plt.plot(xaxis, file_data)
+		plt.xscale('log')
+		text = filename
+		part = text[11:12]
+		prog = text[-5:-4]
+		print part, prog
+		title = "Part"+part+ ": Cumulative distance v/s Access distance for prog"+prog+".c"
+		fig.suptitle(title)
+		plt.xlabel('Access Distance')
+		plt.ylabel('Cumulative Distance Function')
+		plt.savefig(text[10:-4]+'.pdf')
+		# plt.show()
